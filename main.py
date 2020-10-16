613c419c5649828a0d3dff3f5d5d030be51023d5
Harsh Bindal <harshbin06@gmail.com>
2020-10-16 20:22:47 +0530

    layout.html added

--- main.py
+++ main.py
@@ -5,4 +5,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 app.run(debug=True)
